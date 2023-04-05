a5ace76aa8ce6b55fe4ed921ecb6f4263e841b6b
Iryna Pliakhanava <irkapuh@gmail.com>
2023-04-05 21:43:04 +0200

    [1.1:05.04]: fix: fixed the factorializeNumber function so as to receive any numbers

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
 // the first task
 
-checkPrompt();
+checkPrompt1();
 
-function checkPrompt() {
-  const PROMPT = +prompt('Enter a number, please', '');
+function checkPrompt1() {
+  const NUMBER = +prompt('Enter a number, please', '');
 
-  if (
-    !Number.isInteger(PROMPT) ||
-    PROMPT < 0 ||
-    (Number.isNaN(PROMPT) && Number.isFinite(PROMPT))
-  ) {
+  if (!isInteger(NUMBER) || NUMBER < 0 || (isNaN(NUMBER) && isFinite(NUMBER))) {
     console.log('Incorrect input!');
-    checkPrompt();
+    checkPrompt1();
   } else {
     console.log(`
-      Number: ${PROMPT} \n
-      Factorial: ${factorializePrompt(PROMPT)} \n
-      Square: ${Math.pow(PROMPT, 2)} \n
-      isPrime: ${isPrime(PROMPT)} \n
-      isEven: ${isEven(PROMPT)} \n
-      Delimeters: ${findDelimeters(PROMPT)}
+      Number: ${NUMBER} \n
+      Factorial: ${factorializeNumber(NUMBER)} \n
+      Square: ${Math.pow(NUMBER, 2)} \n
+      isPrime: ${isPrime(NUMBER)} \n
+      isEven: ${isEven(NUMBER)} \n
+      Delimeters: ${findDelimeters(NUMBER)}
       `);
   }
 }
 
-function factorializePrompt(num) {
-  if (num < 0) {
-    return -1;
-  } else if (num == 0) {
-    return 1;
-  } else {
-    return num * factorializePrompt(num - 1);
+function isInteger(num) {
+  return Number.isInteger(num);
+}
+
+function isNaN(num) {
+  return Number.isNaN(num);
+}
+
+function isFinite(num) {
+  return Number.isFinite(num);
+}
+
+function factorializeNumber(num) {
+  let result = num;
+
+  if (num === 0 || num === 1) return 1;
+  while (num > 1) {
+    num--;
+    result *= num;
   }
+  return result;
 }
 
 function isPrime(num) {
