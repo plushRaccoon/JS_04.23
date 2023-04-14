945d83af61faa32f9672bebf817ec9adca86501f
Iryna Pliakhanava <irkapuh@gmail.com>
2023-04-14 18:15:04 +0200

    [4.2:14.04]: added the solution of the second task

--- index.js
+++ index.js
@@ -9,3 +9,78 @@ function curry(fn) {
     }
   };
 }
+class Calculator {
+  constructor(num1, num2) {
+    if (
+      !num1 ||
+      !num2 ||
+      typeof num1 !== 'number' ||
+      (isNaN(num1) && !isFinite(num1)) ||
+      typeof num2 !== 'number' ||
+      (isNaN(num2) && !isFinite(num2))
+    ) {
+      throw new Error();
+    } else {
+      this.num1 = num1;
+      this.num2 = num2;
+      this.setX = this.setX.bind(this);
+      this.setY = this.setY.bind(this);
+      this.getSum = this.getSum.bind(this);
+      this.getMul = this.getMul.bind(this);
+      this.getSub = this.getSub.bind(this);
+      this.getDiv = this.getDiv.bind(this);
+    }
+  }
+
+  setX(value) {
+    if (
+      (value !== 0 && !value) ||
+      typeof value !== 'number' ||
+      (isNaN(value) && !isFinite(value))
+    ) {
+      throw new Error();
+    } else {
+      this.num1 = value;
+    }
+  }
+
+  setY(value) {
+    if (
+      (value !== 0 && !value) ||
+      typeof value !== 'number' ||
+      (isNaN(value) && !isFinite(value))
+    ) {
+      throw new Error();
+    } else {
+      this.num2 = value;
+    }
+  }
+
+  getSum() {
+    return this.num1 + this.num2;
+  }
+
+  getMul() {
+    return this.num1 * this.num2;
+  }
+
+  getSub() {
+    return Math.abs(this.num1 - this.num2);
+  }
+
+  getDiv() {
+    if (this.num2 === 0) {
+      throw new Error();
+    } else {
+      return this.num1 / this.num2;
+    }
+  }
+}
+
+function isNaN(num) {
+  return Number.isNaN(num);
+}
+
+function isFinite(num) {
+  return Number.isFinite(num);
+}
