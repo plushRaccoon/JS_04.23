832123bc8c64615b8c69163e83e4e612cbbb5490
Iryna Pliakhanava <irkapuh@gmail.com>
2023-04-26 18:21:36 +0200

    [6.1:26.04]: finished the task

---
+++ index.js
@@ -0,0 +1,215 @@
+'use strict';
+
+const INPUT = document.querySelector('.input'),
+  OUTPUT = document.querySelector('.result'),
+  KEYBOARD = document.querySelector('.keyboard'),
+  SIGNS = ['+', '÷', '×', '-'];
+
+KEYBOARD.addEventListener('click', (event) => {
+  if (!event.target.classList.contains('key')) return;
+  readInput(event);
+});
+
+let current = '';
+let previous = '';
+let action = '';
+let error = false;
+
+function readInput(event) {
+  const TARGET = event.target;
+  if (error) {
+    clear();
+    error = false;
+    OUTPUT.textContent = '';
+    INPUT.textContent = '';
+  }
+  if (TARGET.dataset.key === 'number') {
+    appendNumber(TARGET.textContent);
+    updateDisplay();
+  }
+  if (TARGET.dataset.key === 'action') {
+    chooseAction(TARGET.textContent);
+    updateDisplay();
+  }
+  if (TARGET.dataset.key === 'clear') {
+    clear();
+    updateDisplay();
+  }
+  if (TARGET.dataset.key === 'equal') {
+    calculate();
+    updateDisplay();
+  }
+  if (TARGET.dataset.key === 'plusmin') {
+    changeState();
+    updateDisplay();
+  }
+  if (TARGET.dataset.key === 'del') {
+    del();
+    updateDisplay();
+  }
+}
+
+function appendNumber(number) {
+  if (!validateInput(number, current)) return;
+  if (current.length === 15 && number === '00') {
+    current += 0;
+  } else {
+    current += number;
+  }
+}
+
+function chooseAction(operation) {
+  if (current === '') return;
+  if (previous !== '') {
+    calculate();
+  }
+  action = operation;
+  previous = current;
+  current = '';
+}
+
+function clear() {
+  current = '';
+  previous = '';
+  action = '';
+}
+
+function calculate() {
+  let cur = +parseFloat(current),
+    prev = +parseFloat(previous);
+
+  if (isNaN(prev) || isNaN(cur)) return;
+
+  let computation = compute(cur, prev);
+
+  if (error) {
+    clear();
+    current = "You can't divide by zero";
+    error = true;
+  } else {
+    current = computation;
+    action = '';
+    previous = '';
+  }
+}
+
+function changeState() {
+  const CUR = current.toString();
+  if (CUR.includes('-')) {
+    current = CUR.replace('-', '');
+  } else {
+    current = `-${CUR}`;
+  }
+}
+
+function del() {
+  const CUR = current.toString();
+  current = CUR.slice(0, -1);
+}
+
+function updateDisplay() {
+  OUTPUT.style.fontSize = '2.5rem';
+  if (current > 10000000000 || OUTPUT.textContent.length > 11) {
+    OUTPUT.style.fontSize = '2rem';
+    if (current.length > 1000000000000 || OUTPUT.textContent.length > 13) {
+      OUTPUT.style.fontSize = '1.5rem';
+    }
+  } else {
+    if (error) {
+      OUTPUT.style.fontSize = '1.3rem';
+      OUTPUT.textContent = current;
+    }
+  }
+  OUTPUT.textContent = beautifyNumber(current);
+  if (current > 10000000000 || OUTPUT.textContent.length > 11) {
+    OUTPUT.style.fontSize = '2rem';
+    if (current.length > 1000000000000 || OUTPUT.textContent.length > 13) {
+      OUTPUT.style.fontSize = '1.5rem';
+    }
+  }
+  if (action !== null) {
+    INPUT.textContent = `${beautifyNumber(previous)} ${action}`;
+  } else {
+    INPUT.textContent = '';
+  }
+}
+
+function beautifyNumber(number) {
+  const stringNumber = number.toString(),
+    decimalDigits = stringNumber.split('.')[1];
+
+  let integerDigits, integerDisplay;
+
+  if (
+    +number >= Number.MAX_SAFE_INTEGER ||
+    +number <= Number.MIN_SAFE_INTEGER
+  ) {
+    return (+number).toExponential(8);
+  } else if (
+    +number <= Number.MAX_SAFE_INTEGER ||
+    +number >= Number.MIN_SAFE_INTEGER
+  ) {
+    return stringNumber.replace(/\d{1,3}(?=(\d{3})+(?!\d))/gi, '$& ');
+  } else {
+    integerDigits = parseFloat(stringNumber.split('.')[0]);
+  }
+
+  if (isNaN(integerDigits)) {
+    integerDisplay = '';
+  } else {
+    integerDisplay = integerDigits.toLocaleString('ru');
+  }
+  if (decimalDigits != undefined) {
+    return `${integerDisplay}.${decimalDigits}`;
+  } else {
+    return integerDisplay;
+  }
+}
+
+function compute(cur, prev) {
+  let computation;
+  switch (action) {
+    case '+':
+      computation = prev + cur;
+      break;
+    case '-':
+      computation = prev - cur;
+      break;
+    case '÷':
+      if (cur === 0) {
+        error = true;
+        return false;
+      } else {
+        computation = prev / cur;
+      }
+      break;
+    case '×':
+      computation = prev * cur;
+      break;
+    default:
+      return;
+  }
+  return computation;
+}
+
+function validateInput(value, last) {
+  let maxLength = last.includes('.') ? 17 : 16;
+
+  if (last.length < maxLength) {
+    if (last.includes('.') && value === '.') {
+      return false;
+    }
+
+    if (SIGNS.includes(value)) {
+      if (SIGNS.includes(last)) {
+        return false;
+      } else {
+        return true;
+      }
+    }
+  } else {
+    return false;
+  }
+
+  return true;
+}
